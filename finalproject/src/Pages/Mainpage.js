61c8f123a5f3869d166f973f310bd1db7998f5a7
Berke Akgün <berkeakgun9@gmail.com>
2022-11-04 22:23:31 +0300

    Login Routes Logo
    Login page done. Routes are ready.

---
+++ finalproject/src/Pages/Mainpage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Mainpage.css";
+function Mainpage() {
+  return (
+    <div>
+      <div className="top-bar"></div>
+    </div>
+  );
+}
+export default Mainpage;
--- finalproject/src/Router.js
+++ finalproject/src/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import Mainpage from "./Pages/Mainpage";
 function Routers() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="*" element={<App />} />
+        <Route path="/mainpage" element={<Mainpage />} />
       </Routes>
     </BrowserRouter>
   );
