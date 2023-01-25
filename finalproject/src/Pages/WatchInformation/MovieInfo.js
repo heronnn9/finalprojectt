6bee2769ff76abe8253984491eae33f2cc066b57
Berke Akgün <berkeakgun9@gmail.com>
2023-01-25 22:58:13 +0300

    Last Version
    Last Version

--- finalproject/src/Pages/WatchInformation/MovieInfo.js
+++ finalproject/src/Pages/WatchInformation/MovieInfo.js
@@ -1,13 +1,36 @@
+import axios from "axios";
 import React from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Topbar from "../../Components/Top-Bar/Topbar";
 import "../MoviePage/MovieContainer/MovieContainer";
 import "./MovieInfo.css";
-const MovieInfo = ({ movie }) => {
-  console.log(movie);
+const MovieInfo = () => {
+  const [movieId, setMovieId] = useState([]);
+  const [query, setQuery] = useSearchParams();
+  const { Id } = useParams();
+  const fetchId = async () => {
+    await axios
+      .get(`https://localhost:7298/api/Movie/GetMovieById${Id}`)
+      .then((respo) => {
+        setMovieId(respo.data);
+      });
+  };
+  useEffect(() => {
+    fetchId();
+  });
+
   return (
     <div>
       <Topbar />
-      <div className="Backpath"></div>
+      <div className="Watch">
+        <img
+          className="Backpath"
+          src={`https://image.tmdb.org/t/p/w400${movieId.PosterPath}`}
+          alt="nice"
+        ></img>
+        <div className="Watch-Info">selam</div>
+      </div>
     </div>
   );
 };
